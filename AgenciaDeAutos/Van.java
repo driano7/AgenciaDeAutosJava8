cd58db780b064417b6c9ea1408cf477e7dd426cf
Donovan Riaño <42306682+driano7@users.noreply.github.com>
2021-01-14 08:32:23 -0600

    Add files via upload

---
+++ AgenciaDeAutos/Van.java
@@ -0,0 +1,101 @@
+//package AgenciaDeAutos;
+/**
+ * Clase abstracta de tipo pública para automóviles de tipo Van
+ * @author Riaño Donovan
+ * @author Tapia Alejandro
+ * @author Villaseñor Luis 
+ */
+public abstract class Van{
+    
+    /**
+    * Constructor por defecto de la clase Van
+    */
+    public Van(){}
+        
+    /**
+     *Método abstracto de tipo público para la velocidad de una Van de la marca VolksWagen
+     */
+    public abstract void velocidadVolksWagen();
+
+    /**
+     *Método abstracto de tipo público para el rendimiento de una Van de la marca VolksWagen
+     */
+    public abstract void rendimientoVolksWagen();
+
+    /**
+     *Método abstracto de tipo público para el precio de una Van de la marca VolksWagen
+     */
+    public abstract void precioVolksWagen();
+
+    /**
+     *Método abstracto de tipo público para el sistema de una Van de la marca VolksWagen
+     */
+    public abstract void sistemaVolksWagen();
+	
+    /**
+     *Método abstracto de tipo público para el control de una Van de la marca VolksWagen
+     */
+    public abstract void controlVolksWagen();	
+
+    /**
+     *Método abstracto de tipo público para el equipamiento de una Van de la marca VolksWagen
+     */
+    public abstract void equipadoVolksWagen();
+
+    /**
+     *Método abstracto de tipo público para la velocidad de una Van de la marca Dodge
+     */
+    public abstract void velocidadDodge();
+
+    /**
+     *Método abstracto de tipo público para el rendimiento de una Van de la marca Dodge
+     */
+    public abstract void rendimientoDodge();
+
+    /**
+     *Método abstracto de tipo público para el precio de una Van de la marca Dodge
+     */
+    public abstract void precioDodge();
+
+    /**
+     *Método abstracto de tipo público para el sistema de una Van de la marca Dodge
+     */
+    public abstract void sistemaDodge();
+	
+    /**
+     *Método abstracto de tipo público para el control de una Van de la marca Dodge
+     */
+    public abstract void controlDodge();	
+
+    /**
+     *Método abstracto de tipo público para el equipamiento de una Van de la marca Dodge
+     */
+    public abstract void equipadoDodge();
+
+    /**
+     *Método abstracto de tipo público para la velocidad de una Van de la marca GMC
+     */
+    public abstract void velocidadGMC();
+
+    /**
+     *Método abstracto de tipo público para el rendimiento de una Van de la marca GMC
+     */
+    public abstract void rendimientoGMC();
+
+    /**
+     *Método abstracto de tipo público para el precio de una Van de la marca GMC
+     */
+    public abstract void precioGMC();
+	
+    /**
+     *Método abstracto de tipo público para el control de una Van de la marca GMC
+     */
+    public abstract void controlGMC();	
+
+    /**
+     *Método abstracto de tipo público para el equipamiento de una Van de la marca GMC
+     */
+    public abstract void equipadoGMC();
+
+
+}
